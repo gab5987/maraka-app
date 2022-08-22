37b3f701b82442b73117a440f3eaf162aef02bb7
gab5987 <biell.gb4@gmail.com>
2022-08-22 18:08:17 -0300

    FEAT. Added language switcher

--- src/App.js
+++ src/App.js
@@ -2,33 +2,70 @@ import "./App.css";
 import React, { Component } from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Navegator from "./header/nav/nav";
+import {
+	Nav,
+	NavLink,
+	Bars,
+	NavMenu,
+	NavBtn,
+	NavBtnLink,
+} from './NavbarElements';
 
 var appData = require('./app_data/shared_data.json');
 var ptBR = require('./app_data/res_primaryLanguage.json');
 var enUS = require('./app_data/res_secondaryLanguage.json');
 
-var setLang = true;
-
 const Header = React.lazy(() => import("./header/header.js"));
 
 class App extends Component {
+	constructor() {
+    super();
+    this.state = {
+      language: ptBR
+    };
+  }
+
+	changeLanguagePT = () => this.setState({ language:  ptBR });
+	changeLanguageEN = () => this.setState({ language:  enUS });
 
   render(){
-    var resumeLang = setLang ? enUS : ptBR;
 
     return (
       <div className="App">
-        <Router>
-        <Navegator />
-          <Routes>
-            <Route path='#Header' component={Header} />
-          </Routes>
-        </Router>
+				<Router>
+					<Nav>
+						<Bars />
+
+						<NavMenu>
+							<NavLink to='home' activeStyle> Home </NavLink>
+						</NavMenu>
+
+						<NavBtn>
+								<a onClick={this.changeLanguagePT} className="lang-button">pt-BR 🇧🇷 </a>
+								<a onClick={this.changeLanguageEN} className="lang-button">en-US 🇺🇸 </a>
+						</NavBtn>
+					</Nav>
+
+					<Routes>
+						<Route path='#Header' component={Header}/>
+					</Routes>
+				</Router>
 
         <Header 
           resumeInfo = {appData.info} 
-          resumeLangDeps = {resumeLang}
+          resumeLangDeps = {this.state.language}
+        />
+				<Header 
+          resumeInfo = {appData.info} 
+          resumeLangDeps = {this.state.language}
+        />
+				<Header 
+          resumeInfo = {appData.info} 
+          resumeLangDeps = {this.state.language}
+        />
+				<Header 
+          resumeInfo = {appData.info} 
+          resumeLangDeps = {this.state.language}
         />
       </div>
     );
