429d340395111a00b9863e8d0e8d47340f3c4a3f
gab5987 <biell.gb4@gmail.com>
2022-09-10 20:29:00 -0300

    Enhancement. Changed images in row 1

--- src/rows/row1/row1.js
+++ src/rows/row1/row1.js
@@ -2,8 +2,8 @@ import "./row1.css";
 import Fade from "react-reveal/Fade";
 import { useState, useEffect } from "react";
 
-const photo1 = "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/48/18/38/pousada-maraka.jpg?w=1200&h=-1&s=1";
-const photo2 = "https://cf.bstatic.com/xdata/images/hotel/max1024x768/345694455.jpg?k=3c6982453e50b21a004fbfc743547aca32ea64e8e81ea701c9dcedd113329238&o=&hp=1";
+const photo1 = require("../../app_data/images/ISR_1622.jpg");
+const photo2 = require("../../app_data/images/ISR_1624.jpg");
 const photo3 = "https://cf.bstatic.com/xdata/images/hotel/max1024x768/345694569.jpg?k=712a3b5bc4c38ecab544d4c73e3b49d4d3d361052acadb26611f551b4eba277a&o=&hp=1";
 
 export default function Row1(props) {
@@ -45,7 +45,7 @@ export default function Row1(props) {
       <div className="column-row1-column2">
         <Fade left cascade when={revealimgs}>
           <div className="img-column1">
-            <img className="img1" src={photo1} alt="aa"></img>
+            <img className="img1" src={photo1} alt="aa"></img> <br/>
             <img className="img2" src={photo2} alt="aa"></img>
           </div>
           <div className="img-column2">
