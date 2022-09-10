dd14a0381913a2e180e4f241f34e4997b1403c4f
gab5987 <biell.gb4@gmail.com>
2022-09-10 20:52:34 -0300

    Feat. Added image carrousel

--- src/Images_section/index.js
+++ src/Images_section/index.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import './index.css'
 
-export default function Images() {
-    return (
-        <div>
-            <h1>Still under development...</h1>
-            <a href="/" > go back </a>
-        </div>
-)}
+const handleDragStart = (e) => e.preventDefault();
+const img1 = require("../app_data/images/ISR_1622.jpg");
+const img2 = require("../app_data/images/ISR_1624.jpg")
+
+const items = [
+  <img src={img1} onDragStart={handleDragStart} role="presentation" />,
+  <img src={img2} onDragStart={handleDragStart} role="presentation" />,
+];
+
+const Imgs = () => {
+  return (
+    <div className='imgContainer'>
+        <AliceCarousel autoPlay autoPlayInterval="5000" mouseTracking items={items} />
+    </div>
+  );
+}
+export default Imgs
