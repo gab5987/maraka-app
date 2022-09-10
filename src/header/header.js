b48a6b6de08ebf3f051ae2631d6b227b49b5e79a
gab5987 <biell.gb4@gmail.com>
2022-09-10 16:09:24 -0300

    Feat. Added whatsapp to book button

--- src/header/header.js
+++ src/header/header.js
@@ -5,21 +5,22 @@ import { BrowserView, MobileView } from 'react-device-detect';
 export default function Header(props) {
   var depName = props.resumeInfo.depShortName;
   var bookButton = props.resumeLangDeps.bookButton;
+  const wpbtn = `http://wa.me/${props.resumeInfo.telNumber}?text=${props.resumeInfo.wpText}`
 
   return (
       <div className="header-container">
         <BrowserView>
           <div className="heading-container">
-            <h1>{depName}</h1>
+            <br/><h1>{depName}</h1>
 
-            <a href="#" className="book-button"> {bookButton} </a>
+            <a href={wpbtn} className="book-button"> {bookButton} </a>
           </div>
         </BrowserView>
         <MobileView>
           <div className="mobile-heading-container">
             <h1>{depName}</h1>
 
-            <a href="#" className="book-button"> {bookButton} </a>
+            <a href={wpbtn} className="book-button"> {bookButton} </a>
           </div>
         </MobileView>
       </div>
